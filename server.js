6b6259e05f91fcb8396ede2dc739406225357168
JacobKnaack <jacobknck@gmail.com>
2023-06-27 20:44:57 -0700

    adds create / update / delete handlers

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const bookModel = require('./models/book');
+const getBooks = require('./lib/getBooks');
+const deleteBook = require('./lib/deleteBook');
+const createBook = require('./lib/createBook');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
 
@@ -18,27 +21,6 @@ db.once('open', _ => {
   console.log('We\'re connected!');
 });
 
-async function getBooks(request, response, next) {
-  try {
-    const books = await bookModel.find({});
-    response.status(200).send(books);
-
-    // ERROR HANDLING TESTING PURPOSES ONLY: This shoud cause an error that'll end up in the catch() below and then sent to the middleware in the server.js
-    // let dataThatDoesntExist = require('./this-data-does-not-exist.js');
-    // response.send(dataThatDoesntExist);
-
-  } catch (error) {
-    console.error(error);
-
-    // next can be used to pass an error to express for the error middleware to handle 
-    next(error);
-
-    // THIS is an anti-pattern. DO NOT handle errors inline, this is not the Express way
-    // response.status(400).send('Could not find any books');
-  }
-}
-
-
 app.get('/test', (request, response) => {
 
   response.send('test request received')
@@ -46,6 +28,8 @@ app.get('/test', (request, response) => {
 });
 
 app.get('/books', getBooks);
+app.post('/books', createBook);
+app.delete('/books/:id', deleteBook);
 
 // Not Found
 app.get('*', (request, response) => {
